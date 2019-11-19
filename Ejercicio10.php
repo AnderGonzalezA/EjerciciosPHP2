2ee96915aff240dbdc83c0c06460c4e73da79525
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-11-20 00:08:14 +0100

    Ejercicio 10 mejorado

--- Ejercicio10.php
+++ Ejercicio10.php
@@ -10,6 +10,18 @@
 			setApellido($apellido);
 		}
 
+		public function __get($property) {
+	    if (property_exists($this, $property)) {
+	      return $this->$property;
+	    }
+  	}
+
+  	public function __set($property, $value) {
+	    if (property_exists($this, $property)) {
+	      $this->$property = $value;
+	    }
+		}
+
 		public getDni(){
 			return this.$dni;
 		}
