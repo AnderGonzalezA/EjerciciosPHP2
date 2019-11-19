0310ef62a7dde85d2b3acdee4d0a91ad071285cf
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2019-11-19 23:40:59 +0100

    Ejercicio 7 mejorado

--- Ejercicio7.php
+++ Ejercicio7.php
@@ -1,10 +1,6 @@
 <?php
 	function calcularPotencia($base,$exponente=2){
-		$potencia = $base;
-		for ($i = 0; $i<$exponente-1; $i++){
-			$potencia = $potencia * $base;
-		}
-		return $potencia;
+		return pow($base,$exponente);
 	}
-	echo(calcularPotencia(3,3));
+	echo(calcularPotencia(3,4));
 ?>
